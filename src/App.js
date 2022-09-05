089d671e492476761da0007cbdc10b75a7429bce
Ammar <ammar_98@live.com>
2022-09-05 18:51:54 +0400

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import {nanoid} from 'nanoid';
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
+import Header from "./components/Header";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -28,6 +29,7 @@ const App = () => {
   ]);
 
   const [searchText, setSearchText] = useState('');
+  const [isDarkMode, setDarkMode] = useState(false);
 
   const addNote = (text) => {
     const date = new Date();
@@ -46,9 +48,12 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-      <Search handleSearchNote={setSearchText}/>
-      <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText.toLowerCase()))} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
+    <div className={`${isDarkMode && 'dark-mode'}`}>
+      <div className="container">
+        <Header handleDarkMode={setDarkMode}/>
+        <Search handleSearchNote={setSearchText}/>
+        <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText.toLowerCase()))} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
+      </div>
     </div>
   )
 }
